ffc19b2f52a0c9c0db9e1a59e452f705f5f8061a
ZhiyingLuo <137163779+ZhiyingLuo@users.noreply.github.com>
2025-01-02 23:43:53 +0800

    commit錯誤

--- foodpangolin/app.py
+++ foodpangolin/app.py
@@ -1,7 +1,7 @@
 from flask import Flask , render_template ,request , session , redirect,url_for
 from functools import wraps
 import os
-from Database import get_users_from_db,get_customer_cart_from_sql,add_deliverer_to_db,add_customer_to_db,add_restaurant_to_db,r_getList, r_add, r_editList, r_update, r_delete , r_getallList,r_acceptList,r_announced_deliver,get_r_id,find_today_total_sales,find_today_total_orders,find_finished_order,find_bestsell_and_num,find_comments,find_r_turnover,find_r_turnover_complete_order,find_r_information,update_restaurant_info
+from Database import *
 #app = Flask(__name__ , static_folder = 'static' , static_url_path = '/')
 app = Flask(__name__ , static_folder = 'static/img')
 app.config['SECRET_KEY'] = '123456/-+*'
@@ -36,7 +36,7 @@ def loginforall():
             
             # 根據身份跳轉到不同的主頁
             if identity == '消費客戶':
-                return redirect("/customer_home")  # 跳轉到消費者主頁
+                return redirect("/c_home")  # 跳轉到消費者主頁
             elif identity == '餐廳業者':
                 return redirect('/restaurant_home')  # 跳轉到餐廳業者主頁
             elif identity == '外送人員':
@@ -55,17 +55,37 @@ def logout():
     session.clear()
     return redirect('login_or_register')
 
-@app.route('/customer_home')
-def home1():
+# @app.route('/customer_home')
+# def home1():
+    # if 'loginAcc' not in session:
+    #     return render_template('login_or_register.html')  # 如果未登入，跳轉到登入頁面
+    
+    # account = session['loginAcc']
+    # cart_data = get_customer_cart_from_sql(account, cart_id=1)
+    # if not cart_data:  # 如果 cart_data 是 None 或空
+    #     cart_data = "目前沒有購物車資料"  # 可以顯示一個提示訊息
+
+    # return render_template('user_home2.html', data=cart_data)
+
+
+# 客戶homepage 無任何商品菜單之餐廳不會顯示於列表上
+@app.route('/c_home')
+def c_homepage():
     if 'loginAcc' not in session:
         return render_template('login_or_register.html')  # 如果未登入，跳轉到登入頁面
     
     account = session['loginAcc']
-    cart_data = get_customer_cart_from_sql(account, cart_id=1)
-    if not cart_data:  # 如果 cart_data 是 None 或空
-        cart_data = "目前沒有購物車資料"  # 可以顯示一個提示訊息
+    my_id= get_customer_cart_from_sql(account)
+    if not my_id:  # 如果 my_id 是 None，表示查詢失敗
+        return "查無此帳號，請重新登入", 403  # 回應 403 Forbidden 或引導至登入
+    data=C_gethome()
+	
+    return render_template('c_homepage.html', data=data)
+    
+    #bestsell_and_num = find_bestsell_and_num(my_id)
+    #total_sales = find_today_total_sales(my_id)
 
-    return render_template('user_home2.html', data=cart_data)
+    # return render_template('r_sell_list.html', data=get_order,acc=get_r_id(account))
 
 
 @app.route('/restaurant_home')#新版
@@ -116,8 +136,6 @@ def restaurant_sell_list():
         get_order = "目前沒有購物車資料"  # 顯示提示訊息
 
     return render_template('r_sell_list.html', data=get_order,acc=get_r_id(account))#,data5=bestsell_and_num
-
-
 
 
 @app.route('/delivery_home')
@@ -433,7 +451,148 @@ def r_accept(id):
 def announced(id):
 	dat=r_announced_deliver(id)
 	return redirect(url_for('home2'))
+# ____________________________________________________________________________________________________________________
+#客戶註冊頁面
+# @app.route("/") 
+# def c_register(): 
+# 	return render_template('c_register.html')
 
+#註冊內容儲存至DB
+@app.route("/c_register", methods=['get','post'])
+def c_insertdo():
+	acc=request.form.get('acc')
+	psw=request.form.get('psw')
+	fname=request.form.get('fname')
+	lname=request.form.get('lname')
+	tel=request.form.get('tel')
+	email=request.form.get('email')
+	add=request.form.get('add')
+	C_insertacc(acc, psw, fname, lname, tel, email, add)
+	return render_template('c_register.html')
+
+#客戶homepage #無任何商品菜單之餐廳不會顯示於列表上
+# @app.route("/c_home")
+# def c_homepage():
+# 	data=C_gethome()
+# 	return render_template('c_homepage.html', data=data)
+
+#條件搜尋(下拉)
+@app.route("/search", methods=['get','post'])
+def c_search():
+    option=request.form.get('select')
+    text=request.form.get('text')
+    if (option!=None):
+        data=C_gethome2(option, text)
+        return render_template('c_homepage.html', data=data)
+    elif(text!=""):
+        data=C_gethome2(option, text)
+        return render_template('c_homepage.html', data=data)
+    else:
+        data=C_gethome()
+        return render_template('c_homepage.html', data=data)
+
+#各餐廳菜單
+@app.route("/c_Rmenu/<string:Rname>/<int:Rid>")
+def c_showmanu(Rname, Rid):
+	data=C_getmenu(Rid)
+	fbdata=C_getfeedback(Rid)
+	return render_template('c_Rmanu.html', data=data, Rname=Rname, Rid=Rid, fbdata=fbdata)
+
+#菜品詳細資訊
+@app.route("/c_menuinfo/<string:Rname>/<int:Rid>/<string:Mname>/<int:Mid>")
+def c_showmanuinfo(Rname, Rid, Mname, Mid):
+	data=C_getmenuinfo(Rid, Mid)
+	return render_template('c_Rmanuinfo.html',data=data, Rname=Rname, Rid=Rid, Mname=Mname, Mid=Mid)
+
+#加入購物車
+@app.route("/c_addcar/<string:Rname>/<int:Rid>/<string:Dname>/<int:Dprice>/<int:Num>/<int:Sum>")
+def c_addtocar(Rname, Rid, Dname, Dprice, Num, Sum):
+	data=C_getmenu(Rid)
+	fbdata=C_getfeedback(Rid)
+	C_addcar(Rname, Dname, Dprice, Num, Sum)
+	return render_template('c_Rmanu.html', data=data, Rname=Rname, Rid=Rid, fbdata=fbdata)
+
+#購物車
+@app.route("/c_car/<string:Rname>/<int:Rid>")
+def c_showcar(Rname, Rid):
+	data=C_getcar(Rname)
+	total=C_getcartotal(Rname)['total']
+	return render_template('c_carlist.html', data=data, Rname=Rname, total=total, Rid=Rid)
+
+#購物車：移除
+@app.route("/c_delcar/<string:Rname>/<int:Rid>/<string:Dname>")
+def c_rmcar(Rname, Rid, Dname):
+	C_removecar(Rname, Dname)
+	data=C_getcar(Rname)
+	total=C_getcartotal(Rname)['total']
+	return render_template('c_carlist.html', data=data, Rname=Rname, total=total, Rid=Rid)
+
+#下單
+@app.route("/c_addorder/<string:Rname>")
+def c_addorder(Rname):
+	C_addorder(Rname)
+	data=C_gethome()
+	return render_template('c_homepage.html', data=data)
+
+#已下定清單
+@app.route("/c_orderlist")
+def c_Olist():
+	data=C_getorderlist()
+	return render_template('c_orderlist.html', data=data)
+
+#已下定清單詳細內容
+@app.route("/c_orderlistinfo/<string:Rname>/<int:Oid>")
+def c_Olistinfo(Rname, Oid):
+	data=C_getorderinfo(Rname, Oid)
+	statusid=data[0]['status']
+	status=C_getstatus(statusid)['status']
+	sum=data[0]['sum']
+	data_Dname=data[0]['Dname'].split(",")
+	data_Dprice=data[0]['Dprice'].split(",")
+	data_Num=data[0]['num'].split(",")
+	data2=[]
+	for i in range(len(data_Dname)):
+		data2.append({
+        'Dname': data_Dname[i].strip(),
+        'Dprice': int(data_Dprice[i].strip()),
+        'num': int(data_Num[i].strip())
+    })
+	msg=C_getbtntext(statusid)
+	return render_template('c_orderlistinfo.html', Rname=Rname, sum=sum, status=status, data2=data2, msg=msg, Oid=Oid)
+
+#取消訂單/收貨確認
+@app.route("/c_recievecheck/<string:Rname>/<string:msg>/<int:Oid>")
+def c_check(Rname, msg, Oid):
+	if msg=="取消訂單":
+		C_getbtnfunc(Oid)
+		data=C_gethome()
+		return render_template('c_homepage.html', data=data)
+	else:
+		return render_template('c_recievecheck.html', Rname=Rname, Oid=Oid)
+	
+#不予評價
+@app.route("/c_feedbackNUPL/<int:Oid>")
+def c_nofeedback(Oid):
+	C_getbtnfunc(Oid)
+	data=C_gethome()
+	return render_template('c_homepage.html', data=data)
+
+#前往給予評價
+@app.route("/c_feedbackUPL/<string:Rname>/<int:Oid>")
+def c_feedback(Rname, Oid):
+	C_getbtnfunc(Oid)
+	return render_template('c_feedbackUPL.html', Rname=Rname)
+
+#確實給予評價
+@app.route("/c_insertfeedback/<string:Rname>", methods=['get','post'])
+def c_insertfb(Rname):
+	comment=request.form.get('feedback')
+	rating=request.form.get('rating')
+	rid=C_getrid(Rname)['rid']
+	C_insertfb(rid, rating, comment)
+	data=C_gethome()
+	return render_template('c_homepage.html', data=data)
+# ________________________________________________________________________________________________________________
 # @app.route('/test')
 # def show_list():
 #     data_test = [
